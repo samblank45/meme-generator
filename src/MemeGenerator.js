9b47c840973cf0f66a1c4f127965357c8bf94f42
sam <sblan45@gmail.com>
2021-01-06 23:05:04 -0500

    adds code to save funcitonality

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -16,6 +16,34 @@ class MemeGenerator extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
+
+  async saveImg() {
+
+    let image = document.getElementById('meme-image')
+
+    let canvas = document.getElementById('card-canvas')
+    let canvasRect = canvas.getBoundingClientRect()
+
+    let topText = document.getElementById('top-text')
+    let topTextRect = topText.getBoundingClientRect()
+
+    let bottomText = document.getElementById('bottom-text')
+    let bottomTextRect = bottomText.getBoundingClientRect()
+
+    let ctx = canvas.getContext("2d")
+    ctx.width = canvas.width
+    ctx.height = canvas.height
+
+    ctx.drawImage(image, 0, 0, canvas.witdth, canvas.height )
+    ctx.translate(0, topTextRect.top - canvasRect.top + 15)
+
+
+    let download = document.getElementById('download')
+    let imageDownload = canvas.toDataURL("image/png")
+    download.setAttribute("href", imageDownload)
+  }
+
+
   componentDidMount() {
     this.setState({isLoading: true})
     fetch("https://api.imgflip.com/get_memes")
@@ -54,10 +82,11 @@ class MemeGenerator extends React.Component {
           />
         </form>
           <div  id="container" className="meme">
-            <img src={this.state.randomImg} alt="problem?"/>
+            <img id="meme-image" crossOrigin="Anonymous" src={this.state.randomImg} alt="problem?"/>
             <h2 className="top-text">{this.state.topText}</h2>
             <h2 className="bottom-text">{this.state.bottomText}</h2>
           </div>
+         
         <form className="meme-form" onSubmit={this.handleSubmit}>
           <input
             type="text"
@@ -69,7 +98,14 @@ class MemeGenerator extends React.Component {
           <button>CREATE</button>
         </form>
         <br/>
-          <button>Take PICTURE</button>
+        <a type="button" className="img-button"
+          id="download"
+          href=""
+          download="download.png">
+            Save
+        </a>
+        <canvas id ="card-canvas" style={{position: 'absolute'}}></canvas>
+          
       </div>
     )
   }
